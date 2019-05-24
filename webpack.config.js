31eb56c1cfdb943b5151d2c9313c29fc72e4eb82
Kim Hoang Nguyen <knguyen@lyft.com>
2019-05-24 11:01:38 -0700

    [cleanup] Remove duplicate code

--- webpack.config.js
+++ webpack.config.js
@@ -14,20 +14,3 @@ module.exports = {
         filename: "bundle.js" // Under a file called `lets-lunch/static/bundle.js`.
     }
 };
-
-module.exports = {
-    entry: ["./src/index.jsx"],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"]
-            }
-        ]
-    },
-    output: {
-        path: __dirname + "/static",
-        filename: "bundle.js"
-    }
-};
