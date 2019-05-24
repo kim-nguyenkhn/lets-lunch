2ea58287a26c66e276de657da0764f293c21580d
Kim Hoang Nguyen <knguyen@lyft.com>
2019-05-24 08:33:05 -0700

    Add index.html

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def index():
+    return render_template('templates/index.html')
 
 @app.route('/<name>')
 def hello_name(name):
